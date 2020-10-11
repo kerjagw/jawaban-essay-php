310b20851c32ea71acc3adbbc893faeba5d5fd6e
Rizki Gilang Wijaya <rizkigwganteng@gmail.com>
2020-10-11 17:33:17 +0700

    update no 14

--- nomo-14.php
+++ nomo-14.php
@@ -1,1 +1,17 @@
+<?php
 
+// 14.
+// < Soal essay 4 >
+
+// Bersambung dari < soal essay 3 > Tulislah sintaks SQL untuk menambahkan data pada tabel orders dan customers 
+INSERT INTO customers (nama, email) values 
+	("John Doe", "john@doe.com"),
+	("Jane Doe", "jane@doe.com");
+
+INSERT INTO orders (amount, customer_id) VALUES
+	(500, 1),
+	(200, 2),
+	(750, 2),
+	(250, 1),
+	(400, 2);
+?>
