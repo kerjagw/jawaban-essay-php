76c87e03416e8e689d93759668d63aa14726adf1
Rizki Gilang Wijaya <rizkigwganteng@gmail.com>
2020-10-11 19:11:09 +0700

    update

--- nomor-13.php
+++ nomor-13.php
@@ -1,1 +1,21 @@
+<?php
+function hitung($str){
+$kali = explode('*', $str);
+$bagi  = explode('/', $str);
+$tambah  = explode('+', $str);
+$kurang  = explode('-', $str);
+if (count($kali)==2)
+return $kali[0] * $kali[1];
+if (count($bagi)==2)
+return $bagi[0] / $bagi[1];
+if (count($tambah)==2)
+return $tambah[0] + $tambah[1];
+if (count($kurang)==)
+return $kurang[0] - $kurang[1];
+}
+echo hitung('100*2');
+echo hitung('100/2');
+echo hitung('100+2');
+echo hitung('100-2');
 
+?>
